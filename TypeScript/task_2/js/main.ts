25652f9c7a2c622e3b773ac783985c9af81f3658
SaraEstelle <12152@holbertonstudents.com>
2026-05-21 00:49:32 +0200

    Add Subjects literal type and teachClass function

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -58,3 +58,15 @@ function executeWork(employee: Director | Teacher): string {
 
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+    if (todayClass === "Math") {
+        return "Teaching Math";
+    }
+    return "Teaching History";
+}
+
+console.log(teachClass("Math"));
+console.log(teachClass("History"));
