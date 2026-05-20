0dc9615c657031082514b97f8958776e68550640
SaraEstelle <12152@holbertonstudents.com>
2026-05-20 21:03:37 +0200

    Add StudentClass with constructor and interfaces

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,6 +19,27 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
 
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+    constructor(private firstName: string, private lastName: string) {}
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
 const teacher3: Teacher = {
     firstName: "John",
     lastName: "Doe",
